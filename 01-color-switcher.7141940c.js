f2318f5772a8ece5b809401eac5a121490959b03
Arch819 <116117866+Arch819@users.noreply.github.com>
2023-07-04 09:45:06 +0000

    Deploying to gh-pages from @ Arch819/goit-js-hw-09@7396e4bb35459d7baf4e9be32c586f45192c4db1 🚀

--- 01-color-switcher.7141940c.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){changeStyleId=setInterval((function(){t.disabled=!0,e.disabled=!1,document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(changeStyleId),e.disabled=!0,t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.7141940c.js.map
